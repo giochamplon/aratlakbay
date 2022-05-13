461b3c74e13a23ab3050314d5735b9f5e9115af7
giochamplon <giocarloschamplon@gmail.com>
2022-05-14 00:37:54 +0800

    adding/changing dashboard 3

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,40 +1,45 @@
-import { AppBar, Box, Drawer, IconButton, Toolbar, Typography, ListItem, List, ListItemIcon, ListItemButton, ListItemText, Divider } from '@mui/material';
+import { AppBar, Box, Drawer, IconButton, Toolbar, Typography, ListItem, List, ListItemIcon, ListItemButton, ListItemText, Divider, Avatar, Tooltip } from '@mui/material';
 import React from 'react'
 import Head from 'next/head';
 import Image from 'next/image';
 import { useState } from 'react';
 
 export default function dashboard() {
+  const [open,setOpen] = useState(false);
+  const setDrawerOpen = () => { setOpen(true); };
+  const handleClose = () => { setOpen(false); };
   
-    const [open,setOpen] = useState(false);
-
-    const setDrawerOpen = () => {
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
-  
-    return (
+return (
     <Box>
-      <Head>
-        <title>Admin-Dashboard</title>
-      </Head>
+        <Head>
+          <title>Admin-Dashboard</title>
+        </Head>
 
-      <AppBar>
+        <AppBar>
           <Toolbar>
-              <IconButton onClick={setDrawerOpen}> <Image src= "/assets/svg/home.svg" alt="home" width={30} height={30} /> </IconButton>
+              <IconButton onClick={setDrawerOpen}><Image src= "/assets/svg/home.svg" alt="home" width={30} height={30} /> </IconButton>
               <Typography variant="h4" sx={{marginLeft: "12px"}}>Arat Lakbay</Typography>
           </Toolbar>
-      </AppBar>
+        </AppBar>
 
-      <Drawer anchor ="left" open={open} onClose={handleClose}>
-        <Box sx= {{minWidth: "200px", display: "flex", flexDirection: "column"}}>
+    <Drawer anchor ="left" open={open} onClose={handleClose}>
+        <Box sx= {{minWidth: "200px", height: "100vh", display: "flex", flexDirection: "column"}}>
+        
+        <Box sx={{padding: "12px", display: "flex"}}>
+        <Avatar />
+            <Box sx={{marginLeft: "8px"}}>
+                <Tooltip title= "giocarloschamplon@gmail.com">
+                    <Typography color= "primary" noWrap sx={{maxWidth: "150px"}}>
+                    giocarloschamplon@gmail.com
+                    </Typography>
+                    </Tooltip>
 
-
-
-        <List sx={{marginTop: "55px"}}>
-        <Divider/>    
+                    <Typography color= "secondary">Admin</Typography>
+        </Box>
+        </Box>
+        
+        <List sx={{flexGrow: 1}}>
+            <Divider/>    
             <ListItem button>
                 <ListItemIcon> <Image src= "/assets/svg/profile.svg" alt="profile" width={25} height={25} /> </ListItemIcon>
                 <ListItemText primary= "Profile" />
@@ -54,17 +59,17 @@ export default function dashboard() {
                 <ListItemIcon> <Image src= "/assets/svg/messages.svg" alt="messages" width={25} height={25} /> </ListItemIcon>
                 <ListItemText primary= "Messages" />
             </ListItem>
+            </List>
 
-            <Divider sx={{marginTop: "440px"}} />
+            <List>
+                <Divider/>
             <ListItem button>
                 <ListItemIcon> <Image src= "/assets/svg/settings.svg" alt="settings" width={25} height={25} /> </ListItemIcon>
                 <ListItemText primary= "Settings" />
-            </ListItem>            
-        </List>
-
-
-    </Box>
+            </ListItem>
+            </List>            
+        </Box>
     </Drawer>
     </Box>
-  )
+    )
 }
